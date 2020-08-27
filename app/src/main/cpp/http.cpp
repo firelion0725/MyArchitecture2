1f499ec1bc3c021b08ff88074d5e12c15273b5b9
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-08-27 16:05:46 +0800

    cpp 动态注册

--- app/src/main/cpp/http.cpp
+++ app/src/main/cpp/http.cpp
@@ -5,11 +5,36 @@
 #include <jni.h>
 #include <string>
 
-extern "C"
+#define JNI_REG_CLASS "com/leo/myarchitecture/cpp/CppHttp"
+
 JNIEXPORT jstring JNICALL
-Java_com_leo_myarchitecture_cpp_CppHttp_testString(JNIEnv *env, jobject thiz) {
-    std::string hello = "Hello http cpp";
+testString(JNIEnv *env, jobject thiz) {
+    std::string hello = "Hello test http cpp";
     return env->NewStringUTF(hello.c_str());
+}
+
+static JNINativeMethod g_methods[] = {
+        {"testString", "()Ljava/lang/String;", (void *) testString}
+};
+
+// must define this function
+JNIEXPORT int JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env;
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK) {
+        return JNI_ERR;
+    }
+
+    jclass javaClass = env->FindClass(JNI_REG_CLASS);
+    if (javaClass == NULL) {
+        return JNI_ERR;
+    }
+
+    int method_count = sizeof(g_methods) / sizeof(g_methods[0]);
+    if (env->RegisterNatives(javaClass, g_methods, method_count) < 0) {
+        return JNI_ERR;
+    }
+
+    return JNI_VERSION_1_6;
 }
 
 
